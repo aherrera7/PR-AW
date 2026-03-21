d09564fbd9b8cb154a1e5cd190ff055535e78c7a
Adriana <admuni01@ucm.es>
2026-03-21 18:58:13 +0100

    para que salga 2026 en vez de 2025

--- BistroFDI/includes/vistas/common/footer.php
+++ BistroFDI/includes/vistas/common/footer.php
@@ -1,5 +1,5 @@
 <footer>
     <p>© 2026 BistroFDI</p>
     Universidad Complutense de Madrid
-    <small>&copy; 2025 Mi Primer Sitio Web. Todos los derechos reservados.</small>
+    <small>&copy; 2026 Mi Primer Sitio Web. Todos los derechos reservados.</small>
 </footer>
